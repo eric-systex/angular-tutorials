d10e742a6f6a3c6fd2383180392cf07b2cf0f265
1400404 <eric.lee@systex.com>
2020-03-26 13:51:48 +0800

    init

---
+++ ECApplication/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { appPath } from './app-path.const';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  /**
+   * 給 Template 用的路由定義
+   *
+   * @memberof AppComponent
+   */
+  path = appPath;
+}
---
+++ ECApplication/src/app/product-section/product-section.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductSectionRoutingModule } from './product-section-routing.module';
+import { ProductSectionComponent } from './product-section.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemModule } from '../shared/product-item/product-item.module';
+
+
+@NgModule({
+  declarations: [ProductSectionComponent, ProductListComponent],
+  imports: [
+    CommonModule,
+    ProductSectionRoutingModule,
+    ProductItemModule
+  ]
+})
+export class ProductSectionModule { }
